f137ac06b28041810d6ee15492782a7c33a256f3
Petya1234 <126342444+Petya1234@users.noreply.github.com>
2023-02-26 10:26:30 +0500

    20
    Для игры, описанной в задании 19, найдите минимальное и максимальное значения S, при котором у Пети есть выигрышная стратегия, причем одновременно выполняются два условия:
    
    −  Петя не может выиграть за один ход;
    
    −  Петя может выиграть своим вторым ходом независимо от того, как будет ходить Ваня.
    
    Найденные значения запишите в порядке возрастания без пробелов и разделителей.

--- 20zadanie2.py
+++ 20zadanie2.py
@@ -1,36 +1,36 @@
 def hod1(x, y):
     permit = False
-    if hod2(x + 2, y) == True and x + 2 + y < 94:
+    if hod2(x + 3, y) == True and x + 3 + y < 80:
         permit = True
-    if hod2(x * 4, y) == True and x * 4 + y < 94:
+    if hod2(x * 2, y) == True and x * 2 + y < 80:
         permit = True
-    if hod2(x, y + 2) == True and x + y + 2 < 94:
+    if hod2(x, y + 3) == True and x + y + 3 < 80:
         permit = True
-    if hod2(x, y * 4) == True and x + y * 4 < 94:
+    if hod2(x, y * 2) == True and x + y * 2 < 80:
         permit = True
     return permit
 def hod2(x, y):
     permit = True
-    if hod3(x + 2, y) == False or x + 2 + y >= 94:
+    if hod3(x + 3, y) == False or x + 3 + y >= 80:
         permit = False
-    if hod3(x * 4, y) == False or x * 4 + y >= 94:
+    if hod3(x * 2, y) == False or x * 2 + y >= 80:
         permit = False
-    if hod3(x, y + 2) == False or x + y + 2 >= 94:
+    if hod3(x, y + 3) == False or x + y + 3 >= 80:
         permit = False
-    if hod3(x, y * 4) == False or x + y * 4 >= 94:
+    if hod3(x, y * 2) == False or x + y * 2 >= 80:
         permit = False
     return permit
 def hod3(x, y):
     permit = False
-    if x + 2 + y >= 94:
+    if x + 3 + y >= 80:
         permit = True
-    if x * 4 + y >= 94:
+    if x * 2 + y >= 80:
         permit = True
-    if x + y + 2 >= 94:
+    if x + y + 3 >= 80:
         permit = True
-    if x + y * 4 >= 94:
+    if x + y * 2 >= 80:
         permit = True
     return permit
-for i in range(1, 89+1):
-    if hod1(4, i) == True:
+for i in range(1, 71+1):
+    if hod1(8, i) == True:
         print(i)
